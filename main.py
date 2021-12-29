a0f8086baaee6229841577ee02b9ce02bd2f933d
Axiao89757 <2213231357@qq.com>
2021-12-29 16:07:50 +0800

    更新仓库

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Scrollbar, Label, Text, LEFT, WORD, RIGHT, Y, END, Button, Tk
+from tkinter import *
 import re
 import html
 from urllib import parse
@@ -23,8 +23,11 @@ def translate(text, to_language="auto", text_language="auto"):
 
 # 窗口程序
 window = Tk()
+height = window.winfo_screenheight()
+width = window.winfo_screenwidth()
 window.title('Axiao Google Translate')
-window.geometry('650x600')
+window.geometry(str(int(0.32*width))+'x'+str(int(0.5*height)))
+window.iconbitmap('icon.ico')
 window.wm_attributes('-topmost', 1)
 
 # 三个frame
@@ -39,6 +42,9 @@ f_ed.pack(expand=1)
 scl_org = Scrollbar(f_org)
 scl_ed = Scrollbar(f_ed)
 
+# 初始原文是英文
+org_is_en = True
+
 # 原文文本框
 Label(f_org, text="原文").pack(side=LEFT)
 t_org = Text(f_org, height=12, wrap=WORD)
@@ -50,7 +56,7 @@ t_org.pack()
 
 # 译文文本框
 Label(f_ed, text="译文").pack(side=LEFT)
-t_ed = Text(f_ed, height=12)
+t_ed = Text(f_ed, height=12, )
 scl_ed.pack(side=RIGHT, fill=Y)  # side是滚动条放置的位置，上下左右。fill是将滚动条沿着y轴填充
 scl_ed.config(command=t_ed.yview)  # 将文本框关联到滚动条上，滚动条滑动，文本框跟随滑动
 t_ed.config(yscrollcommand=scl_ed.set)  # 将滚动条关联到文本框
@@ -93,6 +99,10 @@ def translate_ch2en_clip():
 
 
 def wrap():
+    global org_is_en
+    org_is_en = not org_is_en
+    t_org.configure(wrap=WORD if org_is_en else CHAR)
+    t_ed.configure(wrap=CHAR if org_is_en else WORD)
     text_org = t_org.get('0.0', END)
     text_ed = t_ed.get('0.0', END)
     t_org.delete('1.0', END)
@@ -102,15 +112,15 @@ def wrap():
 
 
 # 按钮
-b_en2ch = Button(f_btn, text='英 to 汉', width=10, height=1, command=translate_en2ch)
+b_en2ch = Button(f_btn, text='英to汉', width=8, height=1, command=translate_en2ch)
 b_en2ch.pack(side=LEFT)
-b_en2ch_clip = Button(f_btn, text='英 to 汉（剪切板）', width=20, height=1, command=translate_en2ch_clip)
+b_en2ch_clip = Button(f_btn, text='英to汉(剪切板)', width=13, height=1, command=translate_en2ch_clip)
 b_en2ch_clip.pack(side=LEFT)
-b_wrap = Button(f_btn, text='交换', width=10, height=1, command=wrap)
+b_wrap = Button(f_btn, text='交换', width=7, height=1, command=wrap)
 b_wrap.pack(side=LEFT)
-b_ch2en = Button(f_btn, text='汉 to 英', width=10, height=1, command=translate_ch2en)
+b_ch2en = Button(f_btn, text='汉to英', width=8, height=1, command=translate_ch2en)
 b_ch2en.pack(side=LEFT)
-b_ch2en_clip = Button(f_btn, text='汉 to 英（剪切板）', width=20, height=1, command=translate_ch2en_clip)
+b_ch2en_clip = Button(f_btn, text='汉to英(剪切板)', width=13, height=1, command=translate_ch2en_clip)
 b_ch2en_clip.pack(side=LEFT)
 
 window.mainloop()
