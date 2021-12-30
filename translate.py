d27fe2a979174365f8d7fadb1ffc39fcc0267e43
axiao89757 <2213231357@qq.com>
2021-12-30 13:34:13 +0800

    1. 抽离共同代码作为函数，简化代码 2. 使用strip和replace优化 【交换】 3. 记忆功能

--- translate.py
+++ translate.py
@@ -26,15 +26,17 @@ window = Tk()
 height = window.winfo_screenheight()
 width = window.winfo_screenwidth()
 window.title('Axiao Google Translate')
-window.geometry(str(int(0.32 * width)) + 'x' + str(int(0.5 * height)))
+window.geometry(str(int(0.35 * width)) + 'x' + str(int(0.5 * height)))
 window.iconbitmap('icon.ico')
 window.wm_attributes('-topmost', 1)
 
-# 三个frame
+# 四个frame
 f_org = Frame(window)
 f_org.pack(expand=1)
 f_btn = Frame(window)
 f_btn.pack(expand=1)
+f_ed = Frame(window)
+f_ed.pack(expand=1)
 f_ed = Frame(window)
 f_ed.pack(expand=1)
 
@@ -46,10 +48,11 @@ scl_ed = Scrollbar(f_ed)
 org_is_en = True
 t_list_org = []
 t_list_ed = []
+t_cur_idx = 0
 
 # 原文文本框
 Label(f_org, text="原文").pack(side=LEFT)
-t_org = Text(f_org, height=12, wrap=WORD)
+t_org = Text(f_org, height=10, wrap=WORD)
 scl_org.pack(side=RIGHT, fill=Y)  # side是滚动条放置的位置，上下左右。fill是将滚动条沿着y轴填充
 scl_org.config(command=t_org.yview)  # 将文本框关联到滚动条上，滚动条滑动，文本框跟随滑动
 t_org.config(yscrollcommand=scl_org.set)  # 将滚动条关联到文本框
@@ -58,7 +61,7 @@ t_org.pack()
 
 # 译文文本框
 Label(f_ed, text="译文").pack(side=LEFT)
-t_ed = Text(f_ed, height=12)
+t_ed = Text(f_ed, height=10)
 scl_ed.pack(side=RIGHT, fill=Y)  # side是滚动条放置的位置，上下左右。fill是将滚动条沿着y轴填充
 scl_ed.config(command=t_ed.yview)  # 将文本框关联到滚动条上，滚动条滑动，文本框跟随滑动
 t_ed.config(yscrollcommand=scl_ed.set)  # 将滚动条关联到文本框
@@ -66,40 +69,49 @@ t_ed.configure(font=("Times New Roman", 12))
 t_ed.pack()
 
 
-def translate_en2ch():
-    text = t_org.get('0.0', END).replace('\n', ' ')
+def text_list_append(text_org, text_ed):
+    global t_list_org, t_list_ed, t_cur_idx
+    if len(t_list_org) > 0 and text_org == t_list_org[-1]:
+        return
+    t_list_org.append(text_org)
+    t_list_ed.append(text_ed)
+    t_cur_idx = len(t_list_org) - 1
+    if t_cur_idx > 0:
+        b_last.configure(state=NORMAL)
+        b_next.configure(state=DISABLED)
+
+
+def update_text(text_org, text_ed):
     t_org.delete('1.0', END)
-    t_org.insert(END, text)
-    result = translate(text, "zh-CN", "en")
+    t_org.insert(END, text_org)
     t_ed.delete('1.0', END)
-    t_ed.insert(END, result)
+    t_ed.insert(END, text_ed)
+
+
+def translate_en2ch():
+    text = t_org.get('0.0', END).strip().replace('\n', ' ')
+    result = translate(text, "zh-CN", "en")
+    update_text(text, result)
+    text_list_append(text, result)
 
 
 def translate_ch2en():
-    text = t_org.get('0.0', END).replace('\n', ' ')
-    t_org.delete('1.0', END)
-    t_org.insert(END, text)
+    text = t_org.get('0.0', END).strip().replace('\n', ' ')
     result = translate(text, "en", "zh-CH")
-    t_ed.delete('1.0', END)
-    t_ed.insert(END, result)
+    update_text(text, result)
+    text_list_append(text, result)
 
 
 def translate_en2ch_clip():
-    text = window.clipboard_get().replace('\n', ' ')
-    t_org.delete('1.0', END)
-    t_org.insert(END, text)
+    text = window.clipboard_get().strip().replace('\n', ' ')
     result = translate(text, "zh-CN", "en")
-    t_ed.delete('1.0', END)
-    t_ed.insert(END, result)
+    update_text(text, result)
 
 
 def translate_ch2en_clip():
-    text = window.clipboard_get().replace('\n', ' ')
-    t_org.delete('1.0', END)
-    t_org.insert(END, text)
+    text = window.clipboard_get().strip().replace('\n', ' ')
     result = translate(text, "en", "zh-CH")
-    t_ed.delete('1.0', END)
-    t_ed.insert(END, result)
+    update_text(text, result)
 
 
 def wrap():
@@ -107,15 +119,32 @@ def wrap():
     org_is_en = not org_is_en
     t_org.configure(wrap=WORD if org_is_en else CHAR)
     t_ed.configure(wrap=CHAR if org_is_en else WORD)
-    text_org = t_org.get('0.0', END)
-    text_ed = t_ed.get('0.0', END)
-    t_org.delete('1.0', END)
-    t_org.insert(END, text_ed)
-    t_ed.delete('1.0', END)
-    t_ed.insert(END, text_org)
+    text_ed = t_org.get('0.0', END).strip().replace('\n', ' ')
+    text_org = t_ed.get('0.0', END).strip().replace('\n', ' ')
+    update_text(text_org, text_ed)
+
+
+def left():
+    global t_cur_idx
+    t_cur_idx = t_cur_idx - 1
+    b_next.configure(state=NORMAL)
+    if t_cur_idx == 0:
+        b_last.configure(state=DISABLED)
+    update_text(t_list_org[t_cur_idx], t_list_ed[t_cur_idx])
+
+
+def right():
+    global t_cur_idx, t_list_org
+    t_cur_idx = t_cur_idx + 1
+    b_last.configure(state=NORMAL)
+    if t_cur_idx == len(t_list_org) - 1:
+        b_next.configure(state=DISABLED)
+    update_text(t_list_org[t_cur_idx], t_list_ed[t_cur_idx])
 
 
 # 按钮
+b_last = Button(f_btn, text='<', width=3, height=1, command=left, state=DISABLED, fg="white", bg="DarkGray")
+b_last.pack(side=LEFT)
 b_en2ch_clip = Button(f_btn, text='英to汉(剪切板)', width=13, height=1, fg="white", bg="ForestGreen",
                       command=translate_en2ch_clip)
 b_en2ch_clip.pack(side=LEFT)
@@ -128,9 +157,7 @@ b_ch2en.pack(side=LEFT)
 b_ch2en_clip = Button(f_btn, text='汉to英(剪切板)', width=13, height=1, fg="white", bg="ForestGreen",
                       command=translate_ch2en_clip)
 b_ch2en_clip.pack(side=LEFT)
-b_last = Button(f_btn, text='<', width=3, height=1, command=translate_en2ch)
-b_last.pack(side=LEFT)
-b_next = Button(f_btn, text='>', width=3, height=1, command=translate_en2ch)
+b_next = Button(f_btn, text='>', width=3, height=1, command=right, state=DISABLED, fg="white", bg="DarkGray")
 b_next.pack(side=LEFT)
 
 window.mainloop()
